cda4e8f760a883d1757b5230c645ce17c7ee9022
Isadora Seffair <isadoraseffair@hotmail.com>
2022-10-03 14:13:09 +0100

    Changed background and some other details in CSS

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wedy", "Thu", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
 }
