7a37d847258594d7059d6025fd78b0cfcb4af864
Isadora Seffair <isadoraseffair@hotmail.com>
2022-10-03 13:52:43 +0100

    Looping through forecast variable and displaying real data. Formatted the date, temp and icon for the forecast day

--- src/app.js
+++ src/app.js
@@ -22,35 +22,45 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wedy", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Tue", "Wed", "Thu", "Fri"];
-
   let forecastHTML = `<div class="row">`;
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
               <div class="weather-forecast-date">
-            ${day}
+            ${formatDay(forecastDay.dt)}
             </div>
            <img 
-           src="http://openweathermap.org/img/wn/01d@2x.png" 
+           src="http://openweathermap.org/img/wn/${
+             forecastDay.weather[0].icon
+           }@2x.png" 
            alt="" 
             width=40/>
             <div class="weather-forecast-temp">
               <span class="weather-forecast-temp-max">
-                18º
+                ${Math.round(forecastDay.temp.max)}º
               </span>
               <span class="weather-forecast-temp-min">
-                12º
+                ${Math.round(forecastDay.temp.min)}º
               </span>
         </div>
       </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
