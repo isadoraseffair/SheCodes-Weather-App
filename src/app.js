2ad19b4893b75f93705b77e7581dca037433a1b5
Isadora Seffair <isadoraseffair@hotmail.com>
2022-10-03 13:25:08 +0100

    Added  function to get lon and lat, added forecast API and and logged responde (array of days)

--- src/app.js
+++ src/app.js
@@ -22,7 +22,9 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
+
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Tue", "Wed", "Thu", "Fri"];
@@ -55,6 +57,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -77,10 +86,12 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "f5a08998537384a606dffc14522bc875";
+  let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}
   &appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
@@ -110,8 +121,6 @@ function displayCelciusTemperature(event) {
 }
 
 let celciusTemperature = null;
-
-displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
