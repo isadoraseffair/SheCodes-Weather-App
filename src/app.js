fcd687790a2d6d26f7fe833aae3ef6e750f03e8a
Isadora Seffair <isadoraseffair@hotmail.com>
2022-10-03 12:58:13 +0100

    Added the forecast to JS in loop (forEach) and removed from HTML

--- src/app.js
+++ src/app.js
@@ -22,6 +22,39 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri"];
+
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+              <div class="weather-forecast-date">
+            ${day}
+            </div>
+           <img 
+           src="http://openweathermap.org/img/wn/01d@2x.png" 
+           alt="" 
+            width=40/>
+            <div class="weather-forecast-temp">
+              <span class="weather-forecast-temp-max">
+                18º
+              </span>
+              <span class="weather-forecast-temp-min">
+                12º
+              </span>
+        </div>
+      </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -77,6 +110,8 @@ function displayCelciusTemperature(event) {
 }
 
 let celciusTemperature = null;
+
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
